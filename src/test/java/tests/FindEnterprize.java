7c885b2f4083bac0fe04bcd5fac9239dda712c64
Anna Usolskaya <rakeya@yandex.ru>
2024-03-17 16:04:12 +0300

    Lesson5

---
+++ src/test/java/tests/FindEnterprize.java
@@ -0,0 +1,34 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class FindEnterprize {
+    @BeforeAll
+    static void openGitHab() {
+        //  Configuration.browserSize = "1920*1080";
+        Configuration.baseUrl = "https://github.com/";
+        Configuration.pageLoadStrategy = "eager";
+
+
+    }
+
+    @Test
+    void openPage() {
+
+
+        open("/");
+// Open Enterprise
+        $(".Header-old ").$$("li").findBy(text("Solutions")).hover().$$("li").findBy(text("Enterprise")).click();
+//Open right page
+        $("#hero-section-brand-heading").shouldHave(text("The AI-powered")).shouldHave(visible);
+
+
+    }
+}
+
