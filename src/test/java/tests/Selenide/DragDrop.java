0b60280756b1d93658ef42d6a321f874b2bbcc86
Anna Usolskaya <rakeya@yandex.ru>
2024-03-17 16:04:12 +0300

    Lesson5

---
+++ src/test/java/tests/Selenide/DragDrop.java
@@ -0,0 +1,35 @@
+package tests.Selenide;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.*;
+
+public class DragDrop {
+    @BeforeAll
+    static void openHerokuapp() {
+        //  Configuration.browserSize = "1920*1080";
+        Configuration.baseUrl = "https://the-internet.herokuapp.com/";
+        Configuration.pageLoadStrategy = "eager";
+
+
+    }
+
+    @Test
+    void changingPlaces() {
+
+
+        open("/drag_and_drop");
+     SelenideElement element = $("#column-b");
+        actions().dragAndDropBy(element, 150, 150).perform();
+
+//
+
+
+
+    }
+}
